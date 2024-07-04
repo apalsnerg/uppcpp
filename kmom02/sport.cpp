f7e9bc0ab305d9ba9fce87d2ae13c470a2675116
apalsnerg <birk.hoijer@gmail.com>
2024-07-04 05:24:12 +0200

    Starting assignment 2.

---
+++ kmom02/sport.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int winningRunner;
+
+    int startHour = 0;
+    int startMin = 0;
+    int startSec = 0;
+
+    int endHour = 0;
+    int endMin = 0;
+    int endSec = 0;
+    
+    int currentRunner = 999;
+
+    while (currentRunner > 0) {
+        cout << "Enter a runner number: " << "\n" << ">> ";
+        cin >> currentRunner;
+
+        cout << "Enter the time they started: " << "\n" << ">> ";
+        cin >> startHour >> startMin >> startSec;
+
+        cout << "Enter the time they finished: " << "\n" << ">> ";
+        cin >> endHour >> endMin >> endSec;
+
+        
+    }
+    return 0;
+}
+
+/*
+Start number? 33
+Start time? 17 30 15
+End time? 18 22 35
+Start number? 14
+Start time? 23 35 00
+End time? 0 12 24
+Starting number? 26
+Start time? 10 11 12
+End time? 20 21 22
+Starting number? -1
+Winner is starting number: 14 
+Hour: 0 Min: 37 Sec: 24 
+Number of competitors: 3 
+The program ends
+*/
